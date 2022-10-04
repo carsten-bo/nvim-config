27b27e0fa908386c7dd6ae1804c0d1eba3581a61
Carsten Böhm <carsten.boehm@alexanderthamm.com>
2022-10-04 13:02:05 +0200

    Update plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -78,6 +78,7 @@ return packer.startup(function(use)
   use { "williamboman/nvim-lsp-installer", commit = "e9f13d7acaa60aff91c58b923002228668c8c9e6" } -- simple to use language server installer
   use { "jose-elias-alvarez/null-ls.nvim", commit = "ff40739e5be6581899b43385997e39eecdbf9465" } -- for formatters and linters
   use { "RRethy/vim-illuminate", commit = "c82e6d04f27a41d7fdcad9be0bce5bb59fcb78e5" }
+  use { "neoclide/coc.nvim", branch = "release" }
 
   -- Telescope
   use { "nvim-telescope/telescope.nvim", commit = "d96eaa914aab6cfc4adccb34af421bdd496468b0" }
@@ -105,6 +106,7 @@ return packer.startup(function(use)
   use { "f-person/git-blame.nvim" } 
   use { "karb94/neoscroll.nvim" }
   use { "folke/which-key.nvim" }
+  use { "Pocco81/auto-save.nvim"}
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
